62942efaec228d8e12e6c2367c6342a6278e86c5
Betzalel Kenig <betzalelkenig@gmail.com>
2020-04-27 22:22:18 +0300

    Regexp Basics - is it a vowel?

---
+++ python/7kyu/Regexp Basics - is it a vowel.py
@@ -0,0 +1,4 @@
+# Implement the function which should return true if given object is a vowel (meaning a, e, i, o, u), and false otherwise.
+
+def is_vowel(s): 
+    return s.lower() in ['a', 'e', 'o', 'i', 'u']
