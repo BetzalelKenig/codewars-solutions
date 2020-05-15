f436d8715435f010cdc98d0758d93f497f4e4b65
Betzalel Kenig <betzalelkenig@gmail.com>
2020-05-15 12:37:54 +0300

    add Unique string characters in python

---
+++ python/7kyu/Unique string characters.py
@@ -0,0 +1,17 @@
+# In this Kata, you will be given two strings a and b and your task will be to return the characters that are not common in the two strings.
+
+# For example:
+
+# solve("xyab","xzca") = "ybzc"
+# --The first string has 'yb' which is not in the second string.
+# --The second string has 'zc' which is not in the first string.
+
+# Notice also that you return the characters from the first string concatenated with those from the second string.
+
+# More examples in the tests cases.
+
+# Good luck!
+
+
+def solve(a,b):
+    return f'{"".join([r for r in a if r not in b])}{"".join([r for r in b if r not in a])}'
