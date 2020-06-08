83b89eadb06c008f58d3acdbebf2e6839ddfc38c
Betzalel Kenig <betzalelkenig@gmail.com>
2020-06-08 19:14:49 +0300

    add Greet Me in python

---
+++ python/7kyu/Greet Me.py
@@ -0,0 +1,9 @@
+# Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+# Example:
+
+# "riley" --> "Hello Riley!"
+# "JACK"  --> "Hello Jack!"
+
+def greet(name):
+    return f'Hello {name.capitalize()}!'
