088d104c9edb93b32fcecc54c0ab5dc16ec865b1
BetzalelKenig <59796236+BetzalelKenig@users.noreply.github.com>
2020-08-25 15:39:22 +0300

    add Smallest unused ID in js

---
+++ javascript/8kyu/Smallest unused ID.js
@@ -0,0 +1,15 @@
+// Hey awesome programmer!
+
+// You've got much data to manage and of course you use zero-based and non-negative ID's to make each data item unique!
+
+// Therefore you need a method, which returns the smallest unused ID for your next new data item...
+
+// Note: The given array of used IDs may be unsorted. For test reasons there may be duplicate IDs, but you don't have to find or remove them!
+
+// Go on and code some pure awesomeness!
+
+function nextId(ids) {
+  for (let i = 0; i < Math.max(...ids) + 2; i++) {
+    if (!ids.includes(i)) return i;
+  }
+}
