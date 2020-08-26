9e220dcf996287845c4d2fff547d56b64c583c8f
BetzalelKenig <59796236+BetzalelKenig@users.noreply.github.com>
2020-08-26 15:38:08 +0300

    add Check the exam in js

---
+++ javascript/8kyu/Check the exam.js
@@ -0,0 +1,20 @@
+// The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+// The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+// If the score < 0, return 0.
+
+// For example:
+
+// checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+// checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+// checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+// checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+
+function checkExam(a1, a2) {
+  sum = 0;
+  for (let i = 0; i < a1.length; i++) {
+    sum += a1[i] == a2[i] ? 4 : a2[i] == "" ? 0 : -1;
+  }
+  return sum > 0 ? sum : 0;
+}
