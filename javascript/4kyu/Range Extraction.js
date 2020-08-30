7b4f36c3364e72a1fff5529533e703927a045f94
BetzalelKenig <59796236+BetzalelKenig@users.noreply.github.com>
2020-08-30 17:52:33 +0300

    add Range Extraction in js

---
+++ javascript/4kyu/Range Extraction.js
@@ -0,0 +1,26 @@
+// A format for expressing an ordered list of integers is to use a comma separated list of either
+
+// individual integers
+// or a range of integers denoted by the starting integer separated from the end integer in the range by a dash, '-'. The range includes all integers in the interval including both endpoints. It is not considered a range unless it spans at least 3 numbers. For example ("12, 13, 15-17")
+// Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
+
+// Example:
+
+// solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
+// // returns "-6,-3-1,3-5,7-11,14,15,17-20"
+// Courtesy of rosettacode.org
+
+function solution(list) {
+  let res = [];
+  list.push(null);
+  let count = 0;
+  for (let i = 1; i < list.length; i++) {
+    if (list[i - 1] != list[i] - 1) {
+      i - count > 2
+        ? res.push(`${list[count]}-${list[i - 1]}`)
+        : (res = res.concat(list.slice(count, i)));
+      count = i;
+    }
+  }
+  return res.join();
+}
