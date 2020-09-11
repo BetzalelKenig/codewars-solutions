65434e9f30bf041009d14a550aa3b26ef27f3ea1
Betzalel Kenig <betzalelkenig@gmail.com>
2020-09-11 11:27:30 +0300

    add Grasshopper - Function syntax debugging in typescript

---
+++ tipescript/Grasshopper - Function syntax debugging.ts
@@ -0,0 +1,7 @@
+// Grasshopper - Function syntax debugging
+
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+export function main(verb: string, noun: string): string {
+  return verb + noun;
+}
