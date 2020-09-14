6aba614901a8349f4f51ec4966319d5fc3649cd0
Betzalel Kenig <betzalelkenig@gmail.com>
2020-09-14 21:29:45 +0300

    add Grasshopper - Summation in c

---
+++ c/8kyu/Grasshopper - Summation.c
@@ -0,0 +1,16 @@
+// Summation
+
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+int summation(int num)
+{
+    return (num + 1) * num / 2;
+}
