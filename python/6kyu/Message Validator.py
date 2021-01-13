6832cf6c60d17c3606a87923d32ea8f9920539b0
BetzalelKenig <betzalelkenig@gmail.com>
2021-01-13 22:01:07 +0200

    add Message Validator in python

---
+++ python/6kyu/Message Validator.py
@@ -0,0 +1,18 @@
+# In this kata, you have an input string and you should check whether it is a valid message. To decide that, you need to split the string by the numbers, and then compare the numbers with the number of characters in the following substring.
+
+# For example "3hey5hello2hi" should be split into 3, hey, 5, hello, 2, hi and the function should return true, because "hey" is 3 characters, "hello" is 5, and "hi" is 2; as the numbers and the character counts match, the result is true.
+
+# Notes:
+
+#     Messages are composed of only letters and digits
+#     Numbers may have multiple digits: e.g. "4code13hellocodewars" is a valid message
+#     Every number must match the number of character in the following substring, otherwise the message is invalid: e.g. "hello5" and "2hi2" are invalid
+#     If the message is an empty string, you should return true
+
+import re
+
+def is_a_valid_message(message):
+    arr = re.findall(r'[A-Za-z]+|\d+', message)
+    if len(arr) % 2 != 0:
+        return False
+    return all(arr[i].isdigit() and int(arr[i]) == len(arr[i+1]) for i in range(0,len(arr),2))
