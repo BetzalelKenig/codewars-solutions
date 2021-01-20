4612fc58cf67b2033be91560c0c47dbdee9ecfe5
BetzalelKenig <betzalelkenig@gmail.com>
2021-01-20 10:42:55 +0200

    add Your order, please in python

---
+++ python/6kyu/Your order, please.py
@@ -0,0 +1,13 @@
+# Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+# Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+# If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+# Examples
+
+# "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+# "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+# ""  -->  ""
+
+def order(str):
+    return ' '.join([w[0] for w in sorted(zip(str.split(' '), [n for n in str if n.isdigit()]), key=lambda x: x[1])]) 
