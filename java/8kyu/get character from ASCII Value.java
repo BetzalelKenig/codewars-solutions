41b053f9f6ae85a39413768659a59c5f7c803201
BetzalelKenig <betzalelkenig@gmail.com>
2021-02-03 23:25:49 +0200

    get character from ASCII Value in java

---
+++ java/8kyu/get character from ASCII Value.java
@@ -0,0 +1,11 @@
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// Ascii.getChar(65) // => 'A'
+
+public class Ascii {
+  public static char getChar(int c) {
+    return (char)c;
+  }
+}
