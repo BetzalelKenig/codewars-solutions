3739630611e2cd052bb1ab761247212e7507f218
BetzalelKenig <betzalelkenig@gmail.com>
2021-02-27 21:55:28 +0200

    add Find all active students.sql

---
+++ SQL/8kyu/Find all active students.sql
@@ -0,0 +1,10 @@
+-- Create a simple SELECT query to display student information of all ACTIVE students.
+
+-- TABLE STRUCTURE:
+-- students
+-- Id 	FirstName 	LastName 	IsActive
+
+
+-- Note: IsActive (true or false)
+
+SELECT * FROM students WHERE IsActive = 1;
