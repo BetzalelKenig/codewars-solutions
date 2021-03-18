85288211a8f009a3444da9a0a5c8ca54d8b05ffa
BetzalelKenig <59796236+BetzalelKenig@users.noreply.github.com>
2021-03-18 16:48:13 +0200

    add Replace With Alphabet Position.py

---
+++ python/6kyu/Replace With Alphabet Position.py
@@ -0,0 +1,15 @@
+# Welcome.
+
+# In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+# If anything in the text isn't a letter, ignore it and don't return it.
+
+# "a" = 1, "b" = 2, etc.
+
+# Example
+# alphabet_position("The sunset sets at twelve o' clock.")
+# Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
+def alphabet_position(text):
+    alph = '_abcdefghijklmnopqrstuvwxyz'
+    return ' '.join([str(alph.index(i.lower())) for i in text if i.lower() in alph])
