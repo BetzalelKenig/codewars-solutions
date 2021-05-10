5ae293c4e551dd51648d0e05a6e4e27b5620ebf0
BetzalelKenig <betzalelkenig@gmail.com>
2021-05-10 08:02:19 +0300

    add Sum without highest and lowest number.js

---
+++ javascript/8kyu/Sum without highest and lowest number.js
@@ -0,0 +1,20 @@
+// Sum all the numbers of the array (in F# and Haskell you get a list) except the highest and the lowest element (the value, not the index!).
+// (The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
+
+// Example:
+
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+
+// If array is empty, null or None, or if only 1 Element exists, return 0.
+// Note:In C++ instead null an empty vector is used. In C there is no null. ;-)
+
+// -- There's no null in Haskell, therefore Maybe [Int] is used. Nothing represents null.
+
+let sumArray = (array) =>
+  array && array.length > 2
+    ? array
+        .sort((a, b) => a - b)
+        .slice(1, -1)
+        .reduce((a, b) => a + b)
+    : 0;
