7edf9d1c6f92e0b1ebc326847ac9a303bd6561f3
BetzalelKenig <betzalelkenig@gmail.com>
2021-05-14 15:03:43 +0300

    add Small enough? - Beginner.js

---
+++ javascript/7kyu/Small enough? - Beginner.js
@@ -0,0 +1,5 @@
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+let smallEnough = (a, limit) => Math.max(...a) <= limit;
