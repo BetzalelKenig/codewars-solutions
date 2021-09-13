e28b469764c0767159fd06b96eee2ca014b2a0a9
BetzalelKenig <betzalelkenig@gmail.com>
2021-09-13 13:32:44 +0300

    add Extract the IDs from the data set.js

---
+++ javascript/6kyu/Extract the IDs from the data set.js
@@ -0,0 +1,22 @@
+// Complete the method so that it returns an array of all ID's passed in. The data structure will be similar to the following:
+
+// var data = {
+//   id: 1,
+//   items: [
+//     {id: 2},
+//     {id: 3, items: [
+//       {id: 4},
+//       {id: 5}
+//     ]}
+//   ]
+// }
+
+// extractIds(data) // should return [1,2,3,4,5]
+
+// The method should be able to handle the case of empty data being passed in.
+
+// Note: The only arrays that need to be traversed are those assigned to the "items" property.
+
+function extractIds(data) {
+    return JSON.stringify(data).replace(/[^0-9]/g, '*').split(/\**/).map(x => parseInt(x)).filter(x => !isNaN(x))
+}
