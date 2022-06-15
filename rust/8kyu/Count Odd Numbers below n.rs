c64f3bae8085cf24505acc444b4357d13cee83d5
BetzalelKenig <betzalelkenig@gmail.com>
2022-06-15 14:40:41 +0300

    add Count Odd Numbers below n.rs

---
+++ rust/8kyu/Count Odd Numbers below n.rs
@@ -0,0 +1,11 @@
+// Given a number n, return the number of positive odd numbers below n, EASY!
+// Examples (Input -> Output)
+
+// 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+// 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+
+// Expect large Inputs!
+
+fn odd_count(n: u64) -> u64 {
+    n / 2
+}
